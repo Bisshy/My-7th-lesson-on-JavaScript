851c49e1bb59a743ddc73bbf4290cf3986ae6547
Akinronbi Oluwapelumi <159236972+Bisshy@users.noreply.github.com>
2025-10-18 15:41:58 +0200

    Add files via upload

---
+++ main7.js
@@ -0,0 +1,24 @@
+// //Ternary operator
+
+// let present = true;
+// let food = "yam";
+// let isCustomerBanned = false;
+// let foodAccess = isCustomerBanned ? `sorry, no ${food} for you` : present ? `Yes, there is ${food}` : `sorry, no ${food} for you`
+
+// console.log(foodAccess);
+
+// let score = 60;
+// let grade = score > 89 ? "A" :
+//     score > 79 ? "B" :
+//     score > 69 ? "C" :
+//     score > 59 ? "D" :
+//     "F";
+
+// console.log(`My test grade is a ${grade}. `)
+
+// let player = "rock"
+// let computer = "paper"
+
+// let result = player === computer ? "Tie game" : player === "rock" && computer === "paper" ? "computer wins" : player === "paper" && computer === "scissors" ? "computer wins" : player === "scissors" && computer === "rock" ? "computer wins" : "player wins";
+
+// console.log(result);
